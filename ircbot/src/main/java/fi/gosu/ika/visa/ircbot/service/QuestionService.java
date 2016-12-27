e2b894a6515d459505af1c5bc27894b8a694018f
Ville Nupponen <aikain@gosu.fi>
2016-12-28 00:47:46 +0200

    Fix page count

--- ircbot/src/main/java/fi/gosu/ika/visa/ircbot/service/QuestionService.java
+++ ircbot/src/main/java/fi/gosu/ika/visa/ircbot/service/QuestionService.java
@@ -41,7 +41,7 @@ public class QuestionService {
     }
 
     public Question getRandomQuestion() {
-        Pageable oldest = new PageRequest(0, 1, Sort.Direction.ASC, "lastAsked");
+        Pageable oldest = new PageRequest(0, 10, Sort.Direction.ASC, "lastAsked");
         Page<Question> questions = questionRepository.findAll(oldest);
         Question question = questions.getContent().get((int)(Math.random() * questions.getContent().size()));
         question.setLastAsked(new Date(System.currentTimeMillis()));
